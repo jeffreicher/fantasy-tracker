670aa6f715caa84ce392d2e6f4f854c9d44f6a04
Jeffrei Cher <jeffreicher@yahoo.com>
2018-07-18 13:06:44 -0700

    Added buttons to control date and created div to hold games.No functionality yet.

--- src/components/gamesContainer.js
+++ src/components/gamesContainer.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import '../assets/css/gamesContainer.css';
 
 class GamesContainer extends Component {
     render() {
+        let today = new Date();
+        let day = today.getDate();
+        let month = today.getMonth() +1;
+        let year = today.getFullYear();
+        today = `${month} / ${day} / ${year}`;
         return (
-            <div>
-            Hello 
+            <div className="container gamesContainer">
+                <div className="dateContainer">
+                    <button type="button" className="btn btn-primary">Previous</button>
+                    <div>{today}</div>
+                    <button type="button" className="btn btn-primary">Next</button>                    
+                </div>
             </div>
         );
     };
