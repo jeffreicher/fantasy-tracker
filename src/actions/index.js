220b452d1988865c86506c725fabe4d0c21a3381
Jeffrei Cher <jeffreicher@yahoo.com>
2018-07-19 12:27:08 -0700

    Created reducer file

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,9 +4,21 @@ export const FETCH_GAMES = 'FETCH_GAMES';
 
 const ROOT_URL = 'https://localhost:2424/api/nba';
 
-export function retrieveGames(response) {
+function gamesAction(response) {
     return {
         type: FETCH_GAMES,
         payload: response
+    }
+}
+
+export function retrieveGames(day) {
+    let games = `${ROOT_URL}/games/${day}`;
+    return (dispatch) => {
+        return axios.get(games).then(response => {
+            dispatch(gamesAction(response))
+        }).catch(err => {
+            console.log('error', err);
+            dispatch(renderError(err));
+        })
     }
 }
