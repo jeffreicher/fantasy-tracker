ae7093576c5b2446bfffedcc4c44df80cedc591a
Jeffrei Cher <jeffreicher@yahoo.com>
2018-07-19 12:02:08 -0700

    added react-redux and redux as well as imported into index.js file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './assets/css/index.css';
 import App from './components/App';
 import registerServiceWorker from './helpers/registerServiceWorker';
