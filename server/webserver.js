483f348d0be5163f81bccc07fd9d0853c75d126a
Jeffrei Cher <jeffreicher@yahoo.com>
2018-12-24 08:44:42 -0800

    testing node server

--- server/webserver.js
+++ server/webserver.js
@@ -1,19 +1,42 @@
-const express = require('express');
+const express = require('express')
 // const mysql = require('mysql');
-const nba = require('nba.js').default;
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
+const nba = require('nba.js').default
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const path = require('path')
 
-const app = express();
-const PORT = 2424;
+const app = express()
+const PORT = 2424
 // const credentials = require('./config/mysqlCredentials');
+const NBA = require('nba')
+const curry = NBA.findPlayer('Stephen Curry')
+console.log(curry)
 
-app.use(cors());
-app.use(bodyParser.urlencoded({
+async function stats() {
+  let val = 1
+  console.log(val)
+  const ress = await nba.stats
+    .allPlayers({ IsOnlyCurrentSeason: 0 })
+    .then(data => {
+      res.status(200).json({ Success: true, games: data })
+      return data
+    })
+    .catch(err => {
+      res.status(404).json({ Success: false, Message: '404 Not Found!' })
+      return err
+    })
+  console.log(ress)
+}
+
+stats()
+
+app.use(cors())
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
-app.use(bodyParser.json());
+  })
+)
+app.use(bodyParser.json())
 
 // const connection = mysql.createConnection(credentials);
 
@@ -25,32 +48,68 @@ app.use(bodyParser.json());
 //     }
 // });
 
-app.use(express.static(path.join(__dirname, '..', 'src')));
+// async function getStats() {
+//   try {
+//     let res = await nba.stats.allPlayers(res => {
+//       console.log(res)
+//     })
+//     console.log(res)
+//   } catch (e) {
+//     console.log(e)
+//   }
+//   //   console.log(res)
+// }
+
+// getStats()
+
+// const stats = nba.stats
+//   .allPlayers({ IsOnlyCurrentSeason: 0 })
+//   .then(res => console.log(res))
+//   .catch(err => console.error(err))
+
+// console.log(stats)
+
+// let standings = nba.data.standings((err, res) => {
+//   if (err) {
+//     console.error(err)
+//     return
+//   }
+
+//   console.log(res)
+// })
+
+// console.log(nba.stats.allPlayers().then(data => {
+//     res.status(200).
+// }))
+// console.log(standings)
+app.use(express.static(path.join(__dirname, '..', 'src')))
 
 app.listen(PORT, () => {
-    console.log("Server started on PORT: ", PORT);
-});
+  console.log('Server started on PORT: ', PORT)
+})
 
-app.get('/games/:day', (req,res) => {
-    nba.data.scoreboard({date: req.params.day}).then(data => {
-        res.status(200).json({"Success": true, "games": data})
-    }).catch(err => {
-        res.status(404).json({"Success": false, "Message": "404 Not Found!"})
-    })
-});
-  
-app.get('/calendar', (req,res) => {
-    nba.data.calendar().then(data => {
-      res.status(200).json({"Success": true, "calendar": data})
-    }).catch(err => {
-      res.status(404).json({"Success": false, "Message": "404 Not Found!"})
-    })
-});
+module.exports = app
 
-app.get('/', (req,res) => {
-    nba.data.conferenceStandings().then(data => {
-      res.status(200).json({"Success": true, "games": data})
-    }).catch(err => {
-      res.status(404).json({"Success": false, "Message": "404 Not Found!"})
-    })
-});
+// app.get('/games/:day', (req,res) => {
+//     nba.data.scoreboard({date: req.params.day}).then(data => {
+//         res.status(200).json({"Success": true, "games": data})
+//     }).catch(err => {
+//         res.status(404).json({"Success": false, "Message": "404 Not Found!"})
+//     })
+// });
+
+// app.get('/calendar', (req,res) => {
+//     nba.data.calendar().then(data => {
+//       res.status(200).json({"Success": true, "calendar": data})
+//     }).catch(err => {
+//       res.status(404).json({"Success": false, "Message": "404 Not Found!"})
+//     })
+// });
+
+// app.get('/', (req,res) => {
+//     nba.data.conferenceStandings().then(data => {
+//       res.status(200).json({"Success": true, "games": data})
+//     }).catch(err => {
+//       res.status(404).json({"Success": false, "Message": "404 Not Found!"})
+//     })
+// });
